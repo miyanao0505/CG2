eee75d55f7ea3bab92d1fcb4001241c843f9abf7
miyanao <k023g0050@g.neec.ac.jp>
2024-06-26 12:54:16 +0900

    CG2_05_04_HalfLambert

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -15,7 +15,9 @@ PixelShaderOutput main(VertexShaderOutput input) {
 	float4 textureColor = gTexture.Sample(gSampler, input.texcoord);
 	
 	if (gMaterial.enableLighting != 0) {	// Lightingする場合
-		float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+		// half lambert
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
 		output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
 	}
 	else {	// Lightingしない場合。前回までと同じ演算
