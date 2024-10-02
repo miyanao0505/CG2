1d80f8351a16778fb17a24e8df3d10528e518dc9
miyanao <k023g0050@g.neec.ac.jp>
2024-10-02 13:13:11 +0900

    GE3_03_02_WindowsAPIのクラス化_初期化編1_確認課題

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#include <Windows.h>
-#include <cstdint>
 #include <string>
 #include <format>
 #include <d3d12.h>
@@ -12,10 +10,10 @@
 #include <sstream>
 #include <vector>
 #include "input/Input.h"
+#include "base/WindowsAPI.h"
 #include "Script/MyTools.h"
 #include "Script/Matrix.h"
 #include "Script/MyBase.h"
-#include "externals/imgui/imgui.h"
 #include "externals/imgui/imgui_impl_dx12.h"
 #include "externals/imgui/imgui_impl_win32.h"
 #include "externals/DirectXTex/DirectXTex.h"
@@ -23,26 +21,6 @@
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "dxcompiler.lib")
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-
-// ウィンドウプロシージャ
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-	if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam)) {
-		return true;
-	}
-
-	// メッセージに応じてゲーム固有の処理を行う
-	switch (msg) {
-		//ウィンドウが破棄された
-	case WM_DESTROY:
-		// OSに対して、アプリの終了を伝える
-		PostQuitMessage(0);
-		return 0;
-	}
-
-	// 標準のメッセージ処理を行う
-	return DefWindowProc(hwnd, msg, wparam, lparam);
-}
 
 // 出力ウィンドウに文字を出す
 void Log(const std::string& message) {
@@ -452,22 +430,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Microsoft::WRL::ComPtr<IDXGIFactory7> dxgiFactory;
 	Microsoft::WRL::ComPtr<ID3D12Device> device;
 
-	WNDCLASS wc{};
-	// ウィンドウプロシージャ
-	wc.lpfnWndProc = WindowProc;
-	// ウィンドウクラス名(なんでも良い)
-	wc.lpszClassName = L"CG2WindowClass";
-	// インスタンスハンドル
-	wc.hInstance = GetModuleHandle(nullptr);
-	// カーソル
-	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-
-	// ウィンドウクラスを登録する
-	RegisterClass(&wc);
-
-	// クライアント領域のサイズ
-	const int32_t kClientWidth = 1280;
-	const int32_t kClientHeight = 720;
+	// ポインタ
+	Input* input = nullptr;
+	WindowsAPI* windowsApi = nullptr;
 
 	// ブレンドモード
 	enum BlendMode {
@@ -479,30 +444,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		kBlendModeScreen,		// スクリーン。 Src * (1 - Dest) + Dest * 1
 		kCountOfBlendMode,		// 利用してはいけない
 	};
-
-	// ウィンドウサイズを表す構造体にクライアント領域を入れる
-	RECT wrc = { 0, 0, kClientWidth, kClientHeight };
-
-	// クライアント領域を元に実際のサイズにwrcを変更してもらう
-	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
-
-	// ウィンドウの生成
-	HWND hwnd = CreateWindow(
-		wc.lpszClassName,			// 利用するクラス名
-		L"CG2",						// タイトルバーの文字(何でも良い)
-		WS_OVERLAPPEDWINDOW,		// よく見るウィンドウスタイル
-		CW_USEDEFAULT,				// 表示X座標(Windowsに任せる)
-		CW_USEDEFAULT,				// 表示Y座標(WindowsOSに任せる)
-		wrc.right - wrc.left,		// ウィンドウ横幅
-		wrc.bottom - wrc.top,		// ウィンドウ縦幅
-		nullptr,					// 親ウィンドウハンドル
-		nullptr,					// メニューハンドル
-		wc.hInstance,				// インスタンスハンドル
-		nullptr						// オプション
-	);
-
-	// ポインタ
-	Input* input = nullptr;
 
 #ifdef _DEBUG
 	Microsoft::WRL::ComPtr<ID3D12Debug1> debugController = nullptr;
@@ -690,6 +631,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	input = new Input();
 	input->Initialize(wc.hInstance, hwnd);
 #pragma endregion Input
+
+#pragma region WindowsAPI
+	// WindowsAPIの初期化
+	windowsApi = new WindowsAPI();
+	windowsApi->Initialize();
+#pragma endregion WindowsAPI
 
 	// DescriptorRange作成
 	D3D12_DESCRIPTOR_RANGE descriptorRange[1] = {};
@@ -1164,9 +1111,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ImGui_ImplDX12_Init(device.Get(), swapChainDesc.BufferCount, rtvDesc.Format, srvDescriptorHeap.Get(), srvDescriptorHeap->GetCPUDescriptorHandleForHeapStart(), srvDescriptorHeap->GetGPUDescriptorHandleForHeapStart());
 #endif // _DEBUG
 
-	// ウィンドウを表示する
-	ShowWindow(hwnd, SW_SHOW);
-
 	MSG msg{};
 	// ウィンドウの×ボタンが押されるまでループ
 	while (msg.message != WM_QUIT) {
@@ -1456,6 +1400,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// 入力解放
 	delete input;
+	// WindowsAPIの解放
+	delete windowsApi;
 #ifdef _DEBUG
 	
 #endif // _DEBUG
