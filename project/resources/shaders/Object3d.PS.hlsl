a8fc89afec4b68aae0af8b65bb175b7c50c21b3e
miyanao <k023g0050@g.neec.ac.jp>
2025-01-26 21:04:41 +0900

    CG3_02_01_鏡面反射_Blinn-PhongReflectionModel

--- project/resources/shaders/Object3d.PS.hlsl
+++ project/resources/shaders/Object3d.PS.hlsl
@@ -35,9 +35,9 @@ PixelShaderOutput main(VertexShaderOutput input) {
 		
 		// cameraの方向を産出
 		float32_t3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
-		float32_t3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
-		float RdotE = dot(reflectLight, toEye);
-		float specularPow = pow(saturate(RdotE), gMaterial.shininess);	// 反射強度
+		float32_t3 halfVector = normalize(-gDirectionalLight.direction + toEye);
+        float NDotH = dot(normalize(input.normal), halfVector);
+        float specularPow = pow(saturate(NDotH), gMaterial.shininess); // 反射強度
 		
 		// 拡散反射
 		float32_t3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
