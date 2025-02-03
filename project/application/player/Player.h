3fcc5d3ae2a04492d26d124d4c9c9283b1cd0761
miyanao <k023g0050@g.neec.ac.jp>
2025-02-03 21:43:52 +0900

    関数宣言

--- project/application/player/Player.h
+++ project/application/player/Player.h
@@ -3,6 +3,7 @@
 #include <Input.h>
 #include <list>
 #include "Object3d.h"
+#include "Sprite.h"
 #ifdef _DEBUG
 #include "imgui.h"
 #endif // _DEBUG
@@ -18,9 +19,19 @@ public:	// メンバ関数
 	void Initialize(const std::string& filename);
 
 	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~Player();
+
+	/// <summary>
 	/// 更新
 	/// </summary>
 	void Update();
+
+	/// <summary>
+	/// 旋回
+	/// </summary>
+	void Rotate();
 
 	/// <summary>
 	/// 攻撃
@@ -28,9 +39,22 @@ public:	// メンバ関数
 	void Attack();
 
 	/// <summary>
+	/// 衝突したら呼び出されるコールバック関数
+	/// </summary>
+	void OnCollision() override;
+
+	/// <summary>
 	/// 描画
 	/// </summary>
 	void Draw();
+
+public: // getter
+	MyBase::Vector3 GetWorldPosition() override;
+	MyBase::Vector3 Get3DReticleWorldPosition();
+	
+
+public:	// setter
+	void Set3DReticleFromMouseCursor();
 
 private:	// メンバ変数
 	// オブジェクト
@@ -43,6 +67,9 @@ private:	// メンバ変数
 
 	// 自キャラの速さ
 	const float kCharacterSpeed_ = 0.2f;
+
+	// 3Dレティクル用ワールドトランスフォーム
+
 
 	// その他ステータス
 	int hp_;
